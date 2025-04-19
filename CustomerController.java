1bd1e29bbef46985a989776b56b49e5373e68793
Braeden03 <braeden.carlisle@yahoo.com>
2025-04-18 22:19:30 -0400

    Add files via upload

---
+++ CustomerController.java
@@ -0,0 +1,28 @@
+package com.example.customerapplication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @PostMapping
+    public Customer addCustomer(@RequestBody Customer customer) {
+        return customerRepository.save(customer);
+    }
+}
